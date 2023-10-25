cb1d739ac3f83ec4be79d4924da4ac32b0245881
mickac <kaczynski.mp@gmail.com>
2023-10-26 00:59:34 +0200

    variable adjustment

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ class SpreadSheetMixtin:
         pattern = re.compile(r"0️⃣|1️⃣|2️⃣|3️⃣|4️⃣|5️⃣|6️⃣|7️⃣|8️⃣|9️⃣|🔟")
         anime = pattern.sub("", anime)
         anime_rstrip = anime.rstrip()
-        self.anime = anime.replace("*", "")
+        self.anime = anime_rstrip.replace("*", "")
         self.anime_link = anime_link
         self.season_title = season_title
 
